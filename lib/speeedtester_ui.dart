c6fd265d708f0716110d71617da880e29e35e6ef
ammar <f21bdats1m02054@iub.edu.pk>
2024-05-24 20:52:30 +0500

    Commit

--- lib/speeedtester_ui.dart
+++ lib/speeedtester_ui.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class SpeedTester extends StatefulWidget {
   const SpeedTester({super.key});
@@ -10,13 +11,19 @@ class SpeedTester extends StatefulWidget {
 
 class _SpeedTesterState extends State<SpeedTester> {
   double displayProgress = 0.2;
+  final double _downloadspeed = 0.0;
+  final double _uploadspeed = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 10, 30, 73),
       appBar: AppBar(
-        title: const Text("SpeedTester"),
-        backgroundColor: const Color.fromARGB(255, 10, 35, 73),
+        title: const Text(
+          "SpeedTester",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 10, 30, 73),
         elevation: 0,
         centerTitle: true,
       ),
@@ -45,37 +52,72 @@ class _SpeedTesterState extends State<SpeedTester> {
               barRadius: const Radius.circular(10),
             ),
             const SizedBox(height: 10),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Downloading Speed",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Uploading Speed",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Downloading Speed",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Uploading Speed",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        _downloadspeed.toStringAsFixed(2),
+                        style: const TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        _uploadspeed.toStringAsFixed(2),
+                        style: const TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SfRadialGauge(
+              axes: [
+                RadialAxis(
+                  radiusFactor: 0.90,
+                  minorTicksPerInterval: 1,
+                  tickOffset: 3,
+                  useRangeColorForAxis: true,
+                  showLastLabel: true,
+                  axisLabelStyle: const GaugeTextStyle(color: Colors.white),
+                  interval: 4,
+                  minimum: 0,
+                  maximum: 25,
+                  ranges: [
+                    GaugeRange(
+                      color: const Color.fromARGB(255, 100, 181, 248),
+                      startValue: 0,
+                      endValue: 24,
+                      startWidth: 10,
+                      endWidth: 15,
                     ),
                   ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Downloading Speed",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Uploading Speed",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
+                  pointers: const [
+                    NeedlePointer(
+                      needleColor: Colors.white,
+                    )
                   ],
                 )
               ],
